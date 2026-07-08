1573e19fa93f3084fb59f424e46d32f1e1ce9284
Nick Worthington <nickrworthington@gmail.com>
2026-07-08 12:25:36 +1000

    cards have a quantity input for adding the item contained to the cart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import styles from '../styles/Card.module.css';
-
 const Card = ({ item = { name: 'blank item' } }) => {
+    const [qtyValue, setQtyValue] = useState(null);
+
+
+    function handleQuantityIncrement() {
+        setQtyValue(qty => qty + 1);
+    }
+
+    function handleQuantityDecrement() {
+        if (!qtyValue > 0) return;
+        setQtyValue(qty => qty - 1);
+    }
+
+
     return (
         <div className={styles.card}>
             <header>
@@ -8,6 +21,15 @@ const Card = ({ item = { name: 'blank item' } }) => {
             </header>
             <div>
                 <img src={null} alt={item.name} />
+            </div>
+            <div className={styles.qtyGroup}>
+                <label for='quantity'>Qty:</label>
+                <input type='number' name='quantity' value={qtyValue}></input>
+                <div className={styles.qtyBtns}>
+                    <button onClick={handleQuantityIncrement}>+</button>
+                    <button onClick={handleQuantityDecrement}>-</button>
+                </div>
+                <button>Add</button>
             </div>
         </div>
     );
--- src/components/ItemGroup.jsx
+++ src/components/ItemGroup.jsx
@@ -4,6 +4,7 @@ import Card from './Card';
 const ItemGroup = ({ name = 'blank category' }) => {
     return (
         <>
+            <hr />
             <h3>{name}</h3>
             <div className={styles.itemGroup}>
                 <Card />
