cca6a80518601f9589e1ef861b828ef3fd3b334c
Nick Worthington <nickrworthington@gmail.com>
2026-07-09 12:53:28 +1000

    logo source credit added to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,11 @@ const Footer = () => {
     return (
         <>
             <footer className={styles.footer}>
-                <div></div>
+                <p>Logos sourced from&nbsp;
+                    <a href='https://github.com/google/material-design-icons'>Material Design icons by Google</a>
+                </p>
                 <p>Copyright &copy; Nick Worthington 2026</p>
-                <p>Products supplied by&nbsp;
+                <p>Products sourced from&nbsp;
                     <a href='https://fakestoreapi.com/'>FakeStore API</a>
                 </p>
             </footer>
