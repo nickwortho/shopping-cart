ffaeb50bcd0fe7f86474cbb6021f4662f2d83562
Nick Worthington <nickrworthington@gmail.com>
2026-07-09 11:11:29 +1000

    wrapped NavBar component in header element

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,28 +3,30 @@ import { Link } from 'react-router';
 
 const NavBar = () => {
     return (
-        <nav className={styles.navBar}>
-            <div>
-                <button>
-                    <Link to='/'>Home</Link>
-                </button>
-                <button>
-                    <Link to='/shop'>Shop</Link>
-                </button>
-            </div>
-            <div>
-                <h1>Millard's Mallards</h1>
-            </div>
-            <div>
-                <button>
-                    <img src={null} alt='#' />
-                </button>
-                <button>
-                    <Link to='/cart'>Cart</Link>
-                </button>
-            </div>
+        <header>
+            <nav className={styles.navBar}>
+                <div>
+                    <button>
+                        <Link to='/'>Home</Link>
+                    </button>
+                    <button>
+                        <Link to='/shop'>Shop</Link>
+                    </button>
+                </div>
+                <div>
+                    <h1>Millard's Mallards</h1>
+                </div>
+                <div>
+                    <button>
+                        <img src={null} alt='#' />
+                    </button>
+                    <button>
+                        <Link to='/cart'>Cart</Link>
+                    </button>
+                </div>
 
-        </nav >
+            </nav >
+        </header>
     );
 }
 
